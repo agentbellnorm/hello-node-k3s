5cf2de77eb57030e2970af99d4f4537effde2be4
Morgan Bentell <morgan.bentell@gmail.com>
2022-01-07 17:52:32 +0100

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express'), bodyParser = require('body-parser');
+const app = express()
+
+app.use(bodyParser.json());
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  
+  res.send(JSON.stringify(`Hello ${req.body.name}!`))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
